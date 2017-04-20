7f43a71ed6f1d985b39f7069300dc05cecc41976
Fritz <fre.healy@gmail.com>
2017-04-20 16:52:12 -0400

    styling clinical trials page

--- page-templates/page-clinical-trials.php
+++ page-templates/page-clinical-trials.php
@@ -3,81 +3,81 @@
  * Template Name: Clinical Trials
  */
 
-get_header(); 
-
-
+get_header();
 
 
 ?>
-<div class="wrapper">
+    <div class="wrapper">
 
 
-	<div id="primary" class="content-area-full">
-		<main id="main" class="site-main" role="main">
+        <div id="primary" class="content-area-full">
+            <main id="main" class="site-main template-clinical-trials" role="main">
 
-			<?php
-			while ( have_posts() ) : the_post(); 
+				<?php
+				while ( have_posts() ) : the_post();
 
-			$chair_name = get_field('chair_name');
-			$size ='full';
-			$photo = get_field('chair_photo');
-			$description = get_field('description');
-			$form = get_field('form');
+					$chair_name  = get_field( 'chair_name' );
+					$size        = 'full';
+					$photo       = get_field( 'chair_photo' );
+					$description = get_field( 'description' );
+					$form        = get_field( 'form' );
 
-			?>
-
-				<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-
-					<section class="small-width">
-						<div class="entry-content">
-							<div class="single-board">
-								<div class="left">
-									<div class="member-photo">
-							    		<?php echo wp_get_attachment_image( $photo, $size ); ?>
-							    	</div>
-							    	<h2><?php echo $chair_name; ?></h2>
-							    	<h3>AWIR - Chair of Clinical Trials</h3>
-								</div>
-								<div class="right">
-
-									<?php echo do_shortcode('[gravityform id="' . $form['id'] . '" title="false" description="false" ajax="true"]'); ?>
-								</div>
-							</div>
-						</div><!-- .entry-content -->
-					</section>
-				</article><!-- #post-## -->
-
-				<article id="post-<?php the_ID(); ?>" class="center content-block article hentry">
-					<div class="entry-content">
-						<?php echo $description; ?>
-					</div><!-- .entry-content -->
-				</article><!-- #post-## -->
-
-				<?php if( have_rows('trails') ):  ?>
-					<section class="trials">
-					<?php while( have_rows('trails') ): the_row();
-							$trial_name = get_sub_field('trial_name');
-							$link = get_sub_field('link');
-							$description = get_sub_field('description');
 					?>
 
-					<div class="trial">
-						<h2><?php echo $trial_name; ?></h2>
-						<div class="desc"><?php echo $description; ?></div>
-						<div class="learnmore"><a href="<?php echo $link; ?>">Learn More</a></div>
-					</div>
+                    <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
-						<?php endwhile; ?>
-						</section>
+                        <section class="intro">
+                            <div class="single-board">
+                                <div class="left">
+                                    <div class="left">
+
+                                        <div class="member-photo">
+                                            <?php echo wp_get_attachment_image( $photo, $size ); ?>
+                                        </div><!--.member-photo-->
+                                    </div><!--.left-->
+                                    <div class="right">
+                                        <h2><?php echo $chair_name; ?></h2>
+                                        <h3>AWIR - Chair of Clinical Trials</h3>
+
+                                        <?php echo do_shortcode( '[gravityform id="' . $form['id'] . '" title="false" description="false" ajax="true"]' ); ?>
+                                    </div><!--.right-->
+                                    <div class="clear"></div>
+                                </div><!--.left-->
+                                <div class="right">
+                                    <div class="entry-content">
+                                        <?php echo $description; ?>
+                                    </div><!-- .entry-content -->
+                                </div><!--.right-->
+                                <div class="clear"></div>
+                            </div><!--.single-board-->
+                        </section>
+
+					<?php if ( have_rows( 'trails' ) ): ?>
+                        <section class="trials">
+							<?php while ( have_rows( 'trails' ) ): the_row();
+								$trial_name  = get_sub_field( 'trial_name' );
+								$link        = get_sub_field( 'link' );
+								$description = get_sub_field( 'description' );
+								?>
+
+                                <div class="trial">
+                                    <h2><?php echo $trial_name; ?></h2>
+                                    <div class="desc"><?php echo $description; ?></div>
+                                    <div class="learnmore"><a href="<?php echo $link; ?>">Learn More</a></div>
+                                </div>
+
+							<?php endwhile; ?>
+                        </section>
 					<?php endif; ?>
 
 
-			<?php endwhile; // End of the loop.
-			?>
+                    </article><!-- #post-## -->
+				<?php endwhile; // End of the loop.
+				?>
 
-		</main><!-- #main -->
-	</div><!-- #primary -->
+            </main><!-- #main -->
+        </div><!-- #primary -->
 
-</div>
-<?php 
+    </div>
+<?php
 get_footer();
